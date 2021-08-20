865f08eca6f3bd1c36ecf2eb950a011edda25be7
vinisp <aragao93.vinicius@gmail.com>
2021-08-19 22:29:50 -0300

    mais um pouco do css

--- src/sections/content/index.js
+++ src/sections/content/index.js
@@ -5,12 +5,12 @@ export function Content(){
   return(
   <>
   <main className="content">
-    <section>
+    <section className="textContent">
       <h1>Job name </h1>
         <h3>Subtitle </h3>
           <p>Resume </p>
     </section>
-    <div>
+    <div className="art"> 
         Illustração ou imagem
     </div>
   </main>
