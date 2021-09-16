8d3ddf885d982f08554eb9aa0ccd455c28be9d87
vinisp <aragao93.vinicius@gmail.com>
2021-09-16 15:57:29 -0300

    test deploy

--- src/sections/main/index.js
+++ src/sections/main/index.js
@@ -1,4 +1,5 @@
-import './style.scss'
+import './style.scss';
+import Button from '@material-ui/core/Button'
 
 export function Main(){
   return(
@@ -6,8 +7,8 @@ export function Main(){
     <div className="main">
         <div className="title">
           <h1 className="titleName">FRONT END DEVELOPER</h1>
-            <h2 className="subTitle">Developer better interfaces and sites for your bussines, with moderns technologies, tools, UI and UX for better apresention of you organization.</h2>
-              <button> Contact now ! </button>
+            <h2 className="subTitle">Developer better interfaces and sites for your bussinessss, with moderns technologies, tools, UI and UX for better apresention of you organization.</h2>
+              <Button variant="contained" color="primary"> Contact now ! </Button>
 
         </div>
     </div>
