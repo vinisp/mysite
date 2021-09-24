209477eadad58209adaca9752530b9feed7ea902
vinisp <aragao93.vinicius@gmail.com>
2021-09-24 17:12:35 -0300

    footer icones e info de contato falta icone do whatsapp e email

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -5,12 +5,17 @@ export const Footer = () =>{
   <>
     <footer className="footer">
       <div className="contactInfo">
-
+        <h1> Contato </h1>
+          <h3> Vinicius Silva </h3>
+          <p> Whatsapp: 11 9533-47491 </p>
+            <p> @: contato@silvadev.com </p>
       </div>
       <div className="socialIcons">
+        <div className="linkedIn"> </div>
+        <div className="behance"> </div>
+        <div className="instagram"> </div>
       </div>
     </footer>
-
   </>
   )
 }
